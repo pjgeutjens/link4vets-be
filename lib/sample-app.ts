4abf6e511811148b718061f574a2ec1c5320d5c9
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2021-07-10 11:59:40 +0200

    extra listener and tg

--- lib/sample-app.ts
+++ lib/sample-app.ts
@@ -4,8 +4,10 @@ import { DockerImageAsset } from '@aws-cdk/aws-ecr-assets';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecsp from '@aws-cdk/aws-ecs-patterns';
 import * as ecs from '@aws-cdk/aws-ecs';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as apig from '@aws-cdk/aws-apigatewayv2';
 import { HostedZone } from '@aws-cdk/aws-route53';
+import { FargateService } from '@aws-cdk/aws-ecs';
 
 
 
@@ -63,16 +65,9 @@ export class FargateCluster extends cdk.Construct {
 
       });
 
-      const landingService = new ecsp.ApplicationLoadBalancedFargateService(this, 'LandingService', {
-        cluster: fargateService.cluster,
-        listenerPort: 8080,
-        loadBalancer: fargateService.loadBalancer,
-        serviceName: 'link4vets-landing',
-        domainName: "landing.link4vetsportal.be",
-        domainZone: zone,
-        assignPublicIp: true,
-        taskImageOptions: {
-          image: ecs.ContainerImage.fromDockerImageAsset(landingApp),
+      const landingServiceTask = new ecs.FargateTaskDefinition(this, 'landingServiceTask')
+      const landingContainer = landingServiceTask.addContainer('landingServiceContainer', {
+        image: ecs.ContainerImage.fromDockerImageAsset(landingApp),
           environment: {
             APP_ENVIRONMENT: `env-auth-dev`,
             REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID:"eu-central-1:3ad20eb0-5cf5-4fc2-a5fc-a535801d8140",
@@ -85,8 +80,30 @@ export class FargateCluster extends cdk.Construct {
             REACT_APP_AWS_COGNITO_COOKIE_PATH:"/",
             REACT_APP_AWS_COGNITO_COOKIE_EXPIRES:"7"
           }
-        }
       })
+      landingContainer.addPortMappings({containerPort: 80})
+
+      const landingService = new FargateService(this, 'LandingService', {
+        cluster: fargateService.cluster,
+        taskDefinition: landingServiceTask,
+        desiredCount: 1
+      })
+
+      landingService.connections.allowFrom(fargateService.loadBalancer, ec2.Port.tcp(80))
+      fargateService.loadBalancer.connections.allowTo(landingService, ec2.Port.tcp(80))
+
+      const landingTargetGroup = new elbv2.ApplicationTargetGroup(this, "landingServiceTargetGroup", {
+        targets: [landingService],
+        protocol: elbv2.ApplicationProtocol.HTTP,
+        vpc: props.vpc,
+      });
+
+      fargateService.listener.addTargetGroups('landingTargetGroup', {
+        targetGroups: [landingTargetGroup],
+        hostHeader: "landing.link4vetsportal.be",
+        priority: 100
+    })
+
 
       new cdk.CfnOutput(this, 'SampleAppHost', {
         value: fargateService.loadBalancer.loadBalancerDnsName,
