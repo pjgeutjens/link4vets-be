b86b98e3e062f947178db51a96f854da96cf42cc
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2021-07-10 10:31:33 +0200

    sampleapp

--- lib/sample-app.ts
+++ lib/sample-app.ts
@@ -54,6 +54,25 @@ export class FargateCluster extends cdk.Construct {
 
       });
 
+      const landingService = new ecsp.ApplicationLoadBalancedFargateService(this, 'LandingService', {
+        cluster: fargateService.cluster,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromDockerImageAsset(landingApp),
+          environment: {
+            APP_ENVIRONMENT: `env-auth-dev`,
+            REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID:"eu-central-1:3ad20eb0-5cf5-4fc2-a5fc-a535801d8140",
+            REACT_APP_AWS_COGNITO_REGION:"eu-central-1",
+            REACT_APP_AWS_PROJECT_REGION:"eu-central-1",
+            REACT_APP_AWS_USER_POOLS_ID:"eu-central-1_9RFaWebkP",
+            REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID:"46fe5t3p73fphf6dm72kkqsv78",
+            REACT_APP_AWS_COGNITO_COOKIE_DOMAIN:"link4vetsportal.be",
+            REACT_APP_AWS_COGNITO_COOKIE_SECURE:"true",
+            REACT_APP_AWS_COGNITO_COOKIE_PATH:"/",
+            REACT_APP_AWS_COGNITO_COOKIE_EXPIRES:"7"
+          }
+        }
+      })
+
       new cdk.CfnOutput(this, 'SampleAppHost', {
         value: fargateService.loadBalancer.loadBalancerDnsName,
         exportName: 'SampleAppHost'
